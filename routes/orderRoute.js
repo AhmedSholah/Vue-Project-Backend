5b1e3cd345aee2aff85acbc7f381f37cc90fc55d
Ahmed <midots202@gmail.com>
2025-04-21 14:44:50 +0200

    -

--- routes/orderRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { protect } = require("../controllers/authController");
-const deleteOrder = require("../controllers/orderController");
-const router = express.Router();
-
-router.delete("/:id", protect, deleteOrder);
-
-module.exports = router;
